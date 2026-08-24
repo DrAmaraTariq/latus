1020195e9519274c9c7fffc9cde982d5e1f11b2a
AMMARA TARIQ <alarat3311@gmail.com>
2026-08-24 14:36:56 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Appointments from "./pages/Appointments";
@@ -7,17 +8,21 @@ import Consultation from "./pages/Consultation";
 import Messages from "./pages/Messages";
 import Reports from "./pages/Reports";
 
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/latus">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
         <Route path="/appointments" element={<Appointments />} />
         <Route path="/patients" element={<Patients />} />
         <Route path="/patients/:patientId" element={<PatientIntelligence />} />
         <Route path="/consultation/:patientId" element={<Consultation />} />
         <Route path="/messages" element={<Messages />} />
         <Route path="/reports" element={<Reports />} />
+     
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/patients/:patientId" element={<PatientIntelligence />} />
       </Routes>
     </BrowserRouter>
   );
